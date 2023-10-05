24730fe59edef4d2c54edea03d67df4a87ef2013
MarianPoirot <81marianp@gmail.com>
2023-10-05 16:07:13 +0200

    Init project

---
+++ Scripts/UI/Opening.gd
@@ -0,0 +1,28 @@
+extends Node
+
+func _physics_process(_delta):
+	if Input.is_action_pressed("start"):
+		_on_start_button_button_down()
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_start_button_button_down():
+	pass # Replace with function body.
+
+
+func _on_language_option_button_item_selected(index):
+		match index:
+			0:
+				TranslationServer.set_locale("en")
+			1:
+				TranslationServer.set_locale("fr")
+			2:
+				TranslationServer.set_locale("tl")
