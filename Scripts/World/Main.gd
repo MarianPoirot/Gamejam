8fffca12f423e9f0dadfe1b5bce3c0c4c27aa2e6
MarianPoirot <81marianp@gmail.com>
2023-10-08 16:06:00 +0200

    Updated map to be only a map and not a player anymore, and added music

--- Scripts/World/Main.gd
+++ Scripts/World/Main.gd
@@ -5,12 +5,14 @@ var score : int
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	score = 0
+	$AudioStreamPlayer.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	pass
 
 func _on_player_is_dead():
+	$AudioStreamPlayer2D.stop()
 	if get_tree().change_scene_to_file("res://Scenes/UI/Ending.tscn") != OK:
 		print ("Error passing from Main scene to Ending scene")
 
